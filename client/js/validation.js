bd4245ef28d13c7e983b7b47d569fe95d2530765
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2022-10-29 12:42:20 +0530

    basic validation complete

--- client/js/validation.js
+++ client/js/validation.js
@@ -1,81 +1,156 @@
-    function ValidateForm() {
-      this.isValid = false;
-      this.schema = {
-        name: {
-          ref: name,
-          type: "alpha-only",
-          len: [5, 50]
-          required: true
-        },
-        college: {
-          ref: college,
-          type: "alpha-only",
-          len: [5, 100]
-          required: true
+function ValidateForm() {
+  this.isValid = true;
+  this.schema = {
+    studentName: {
+      ref: studentName,
+      type: "alpha-only",
+      len: [5, 50],
+      required: true,
+      error: null,
+    },
+    college: {
+      ref: college,
+      type: "string",
+      len: [5, 100],
+      required: true,
+      error: null,
+    },
+    sem: {
+      ref: sem,
+      type: "number",
+      range: [1, 2],
+      required: true,
+      error: null,
+    },
+    feesMonth: {
+      ref: feesMonth,
+      type: "month",
+      required: true,
+      error: null,
+    },
+    paymentMode: {
+      ref: paymentMode,
+      type: "string",
+      required: true,
+      error: null,
+    },
+    mobile: {
+      ref: mobile,
+      type: "tel",
+      required: true,
+      error: null,
+    },
+    gurdianMobile: {
+      ref: gurdianMobile,
+      type: "tel",
+      required: true,
+      error: null,
+    },
+    screenshot: {
+      ref: screenshot,
+      type: "file",
+      allowedFile: "jpg|jpeg|png",
+      required: true,
+      error: null,
+    },
+  };
 
-        },
-        sem: {
-          ref: college,
-          type: "number",
-          range: [1, 2],
-          required: true
-        },
-        feesMonth: {
-          ref: feesMonth,
-          type: "month",
-          required: true
-        },
-        paymentMode: {
-          ref: paymentMode,
-          type: "string",
-          required: true
-        },
-        mobile: {
-          ref: mobile,
-          type: "tel",
-          required: true
-        },
-        gurdianMobile: {
-          ref: gurdianMobile,
-          type: "tel",
-          required: true
-        },
-        screenshot: {
-          ref: screenshot,
-          type: "file",
-          allowedFile: "jpg|jpeg|png",
-          required: true
-        }
-      }
-      this.validate = function() {
-        console.log(this.schema)
-        for(const inputField in this.schema) {
-          for(const prop in this.schema[inputField]) {
-            const value = ref.value;
-            if(prop.type)
-              validateType(value, prop);
-            if(prop.required)
-              validateRequired(value);
-            if(prop.len)
-              validateLen(value, prop);
-            if(prop.range)
-              validateRange(value, prop);
-          }
-        }
-    }
-    let validateRequired = function(value, prop) {
-      if(value.trim().length >= 1) 
-        return null;
-      return "Required Field!";
-    }
-    let validateType = function(value, prop) {
-      if(type === "alpha-only") {
-        if(value.match(/[^a-zA-Z]/)) return "Only Alphabet allowed!";;
-        return null;
-      }
-      if(type === "number") {
-        if(value.match(/[^0-9]/)) return "Only numbers are allowed";
-      } 
-    }
+  this.validate = function () {
+    console.log(this.schema);
 
+    for (const inputField in this.schema) {
+      const value = this.schema[inputField].ref.value;
+      //console.log(this.schema[inputField]);
+      this.schema[inputField].error = { exists: false };
+      for (const prop in this.schema[inputField]) {
+        if (prop === "type")
+          this.schema[inputField].error.type = validateType(
+            this.schema[inputField].type,
+            value
+          );
+        if (prop === "required")
+          this.schema[inputField].error.required = validateRequired(
+            this.schema[inputField].required,
+            value
+          );
+        if (prop === "len")
+          this.schema[inputField].error.len = validateLen(
+            this.schema[inputField].len,
+            value
+          );
+        if (prop === "range")
+          this.schema[inputField].error.range = validateRange(
+            this.schema[inputField].range,
+            value
+          );
+      }
+      if (
+        this.schema[inputField].error.type ||
+        this.schema[inputField].error.required ||
+        this.schema[inputField].error.len ||
+        this.schema[inputField].error.range
+      ) {
+        this.schema[inputField].error.exists = true;
+      }
+      //console.log(this.schema[inputField]);
+
+      this.isValid = !this.schema[inputField].error.exists && this.isValid;
+    }
+  };
+  let validateRange = function ([upper, lower], value) {
+    if (value >= upper && value <= lower) return null;
+    return `The value should be between ${upper} and ${lower}`;
+  };
+  let validateLen = function ([min, max], value) {
+    if (value.toString().length >= min && value.toString().length <= max)
+      return null;
+    return `The length must be between ${min} and ${max}`;
+  };
+  let validateRequired = function (isRequired, value) {
+    if (!isRequired) return null;
+    if (value.trim().length >= 1) return null;
+    return "Required Field!";
+  };
+  let validateType = function (type, value) {
+    if (type === "alpha-only") {
+      if (value.match(/[^a-zA-Z\s]/)) return "Only Alphabet allowed!";
+      return null;
+    }
+    if (type === "number") {
+      if (value.match(/[^0-9]/)) return "Only numbers are allowed";
+      return null;
+    }
+    if (type === "tel") {
+      if (value.toString().length != 10 || value.match(/[^0-9]/))
+        return "Please enter a valid number";
+      return null;
+    }
+    if (type === "month") return null;
+  };
+  this.showErrors = function () {
+    console.clear();
+
+    const errorList = document.createElement("ul");
+    for (const inputField in this.schema) {
+      const field = this.schema[inputField];
+      const feedback = document.querySelector(`#${field.ref.id}-fb`);
+      console.log(inputField, field.error);
+      if (!field.error.exists) {
+        feedback.innerHTML = "Looks good!";
+        field.ref.classList.remove("is-invalid");
+        field.ref.classList.add("is-valid");
+      } else {
+        field.ref.focus();
+        for (const key in field.error) {
+          if (key === "exists") continue;
+          const item = document.createElement("li");
+          item.innerHTML = field.error[key];
+          errorList.appendChild(item);
+        }
+        feedback.appendChild(errorList);
+        field.ref.classList.add("is-invalid");
+        field.ref.classList.remove("is-valid");
+      }
+    }
+  };
 }
